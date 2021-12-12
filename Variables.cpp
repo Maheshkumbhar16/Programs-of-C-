b50ec77af00e7ac12cfc463be73f4068f6926288
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-12 11:47:46 +0530

    Variables

---
+++ Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int mynum = 5;
+    double myfloat = 5.1234;
+    char myletter = 'a';
+    string mytext = "Hello";
+    bool mybool = true;
+    cout << mynum << endl;
+    cout << myfloat << endl;
+    cout << myletter <<endl;
+    cout << mytext <<endl;
+    cout << mybool <<endl;
+return 0;
+}
