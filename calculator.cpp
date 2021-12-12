e24c8e452ac8a2a26089ef16ea970b744ae6c43d
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-12 11:59:53 +0530

    Basic example of Calculator
    Basic Arithmetic Operators are used

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Enter numbers are \n" <<num1 <<endl <<num2 <<endl;
+    cout << "addition is : " << num1 + num2 <<endl;
+    cout << "subtraction is : " << num1 - num2 <<endl;
+    cout << "Multiplication is : " << num1 * num2 <<endl;
+    cout << "Division is : " << num1/num2 <<endl;
+    cout << "Module is : " << num1%num2 ;
+return 0;
+}
