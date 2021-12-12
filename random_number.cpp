21e18c07f1f12f94584016966bf84ec5c2754433
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-12 11:24:50 +0530

    This program is to print random values
    The output is always same whenever the program runs

---
+++ random_number.cpp
@@ -0,0 +1,12 @@
+//This program is to print random values
+//The output is always same whenever the program runs
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i<=5; i++){
+        cout<< rand()<<endl; //rand() is used for print random values
+    }
+return 0;
+}
